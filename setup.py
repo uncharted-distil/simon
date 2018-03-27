1456550838a433c26e63fffec8308638525bc894
Ubuntu <paul@newknowledge.io>
2018-03-27 18:32:56 +0000

    first attempt at pip installable class

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='Simon',
+    version='1.1.0',
+    description='Character-level CNN+LSTM model for text classification',
+    packages=['Simon'],
+    install_requires=['Faker >= 0.7.7',
+        'Keras >= 2.0.2',
+        'scikit-learn >= 0.18.1',
+        'python-dateutil >= 2.5.3',
+        'pandas >= 0.19.2',
+        'scipy >= 0.19.0',
+        'tensorflow >= 1.1.0',
+        'h5py >= 2.7.0'],
+)
