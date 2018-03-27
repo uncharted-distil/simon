f5375df49de20aae6d718f22a7a8543e9fbb8897
Ubuntu <paul@newknowledge.io>
2018-03-27 18:56:07 +0000

    removing some redundant stuff

--- azure_utils/adl.py
+++
@@ -1,9 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-
-
-def put_dir(client, local_dir_path, destination_dir):
-    files = [f for f in listdir(local_dir_path) if isfile(join(local_dir_path, f))]
-
-    for f in files:
-        client.put(join(local_dir_path, f), join(destination_dir, f))
--- azure_utils/config.py
+++
@@ -1,4 +0,0 @@
-try:
-    from azure_utils.local_config import *
-except ImportError:
-    pass
