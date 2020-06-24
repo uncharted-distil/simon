09309514d31574833543de376e0d99c1b9bbfe54
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2020-06-24 13:34:12 -0500

    bump to tf 2.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='Simon',
         'python-dateutil==2.8.1',
         'pandas==1.0.3',
         'scipy==1.4.1',
-        'tensorflow-gpu==2.1.0',
+        'tensorflow-gpu==2.2.0',
         'h5py >= 2.7.0'],
     include_package_data=True,
 )
